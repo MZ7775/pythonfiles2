5dfc16fd97cd28313c958f029825de6b4934382f
MZ7775 <mubashirzakir15@gmail.com>
2025-03-29 23:21:20 +0500

    Add files via upload

---
+++ elif123.py
@@ -0,0 +1,21 @@
+# appleprice=500
+# budget=500
+# if(budget-appleprice>200):
+#     print("buy")
+# elif(budget-appleprice>100):
+#     print("we shall buy")
+# else:
+#     print("do not buy")
+
+num=80
+if (num<0):
+    print("number is negative")
+elif (num>0):
+    if (num<=10):
+        print("number is between 1-10")
+    elif (num>10 and num<=20):
+        print("number is between 11-20")
+    else:
+        print("num>20")
+else:
+    print("number is zero")
